5328d1ad5e00fb8a0eecbed1c45f336915a98479
Rick Ucker <rick.ucker@gmail.com>
2017-08-15 00:52:57 -0400

    Handle directories with spaces

--- fix-the-fox.sh
+++ fix-the-fox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 FIREFOX_DIRS=()
-for dir in ${FIREFOX_DIRS[@]}; do
-  cd $dir
+for dir in "${FIREFOX_DIRS[@]}"; do
+  cd "${dir}"
   cp firefox firefox.bak
   mv firefox firefox-bin
   cp firefox.sh firefox
